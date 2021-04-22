bc1d5a681d860050b1a0f1c805c368a4e018d922
minhthanh3145 <mitatoto1997@gmail.com>
2021-04-22 17:10:16 +0700

    Refactor

--- src/background/background.js
+++ src/background/background.js
@@ -1,62 +1,46 @@
 import("../io/itemDB.js").then(({ ItemDB }) => {
   import("../constants.js").then(({ Constants }) => {
-    showMarkItemAsToReadNotification(
-      ItemDB.addItemToDatabase,
-      Constants,
-      ItemDB.getRandomId
-    );
-    showMarkItemAsToDoNotification(
-      ItemDB.addItemToDatabase,
-      Constants,
-      ItemDB.getRandomId
-    );
+    showMarkItemAsToReadNotification();
+    showMarkItemAsToDoNotification();
+
+    function showMarkItemAsToDoNotification() {
+      chrome.contextMenus.create({
+        title: "Mark as To Do",
+        contexts: ["page", "selection", "image", "link"],
+        onclick: (info) => {
+          ItemDB.addItemToDatabase(
+            Constants.TO_DO,
+            ItemDB.getRandomId(),
+            info.selectionText ? info.selectionText : ""
+          );
+          chrome.notifications.create("", {
+            iconUrl: "../../images/icon/list.png",
+            type: "basic",
+            title: "Marked as To Do",
+            message: "A page has been added to To Do list",
+          });
+        },
+      });
+    }
+
+    function showMarkItemAsToReadNotification() {
+      chrome.contextMenus.create({
+        title: "Mark as To Read",
+        contexts: ["page", "selection", "image", "link"],
+        onclick: (info, tab) => {
+          ItemDB.addItemToDatabase(
+            Constants.TO_READ,
+            ItemDB.getRandomId(),
+            "[" + tab.title + "](" + info.pageUrl + ")"
+          );
+          chrome.notifications.create("", {
+            iconUrl: "../../images/icon/list.png",
+            type: "basic",
+            title: "Marked as to read",
+            message: "A page has been added to To Read list",
+          });
+        },
+      });
+    }
   });
 });
-
-function showMarkItemAsToDoNotification(
-  addItemToDatabase,
-  Constants,
-  getRandomId
-) {
-  chrome.contextMenus.create({
-    title: "Mark as To Do",
-    contexts: ["page", "selection", "image", "link"],
-    onclick: (info) => {
-      addItemToDatabase(
-        Constants.TO_DO,
-        getRandomId(),
-        info.selectionText ? info.selectionText : ""
-      );
-      chrome.notifications.create("", {
-        iconUrl: "../../images/icon/list.png",
-        type: "basic",
-        title: "Marked as To Do",
-        message: "A page has been added to To Do list",
-      });
-    },
-  });
-}
-
-function showMarkItemAsToReadNotification(
-  addItemToDatabase,
-  Constants,
-  getRandomId
-) {
-  chrome.contextMenus.create({
-    title: "Mark as To Read",
-    contexts: ["page", "selection", "image", "link"],
-    onclick: (info, tab) => {
-      addItemToDatabase(
-        Constants.TO_READ,
-        getRandomId(),
-        "[" + tab.title + "](" + info.pageUrl + ")"
-      );
-      chrome.notifications.create("", {
-        iconUrl: "../../images/icon/list.png",
-        type: "basic",
-        title: "Marked as to read",
-        message: "A page has been added to To Read list",
-      });
-    },
-  });
-}
